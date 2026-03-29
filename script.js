7476562ff325239f455dcdc7e483769c4a2afcad
Giorgio Torricelli <torricelligiorgio7@gmail.com>
2026-03-29 18:29:30 +0200

    completato esercizio 3

--- script.js
+++ script.js
@@ -18,22 +18,37 @@
 // Dato un array, fai in modo di trovare attraverso un for un valore specificato dall'utente. 
 // Restituisci come risposta un alert o console.log che dica l'indice dell'elemento
 
-const valori = ["tonno", "carpa", "anguilla", "salmone", "trota"];
-let rispostaUtente = prompt("Controlla se il pesce è nella lista:");
+// const valori = ["tonno", "carpa", "anguilla", "salmone", "trota"];
+// let rispostaUtente = prompt("Controlla se il pesce è nella lista:");
 
-for (let i = 0; i < valori.length -1, valori[i -1] !== rispostaUtente; i++) { //ho utilizzato il for senza break
+// for (let i = 0; i < valori.length -1, valori[i -1] !== rispostaUtente; i++) { //ho utilizzato il for senza break
     
-    if (rispostaUtente === valori[i]) {
-        console.log(`${rispostaUtente} è nella lista all'indice ${valori.indexOf(rispostaUtente)}`);
-    } 
+//     if (rispostaUtente === valori[i]) {
+//         console.log(`${rispostaUtente} è nella lista all'indice ${valori.indexOf(rispostaUtente)}`);
+//     } 
     
 
-}
+// }
 
 
 // Dato un array di numeri, scrivi il codice che effettua la somma di tutti i valori. 
 // BONUS: fai generare l'array all'utente tramite prompt. 
 // nel momento in cui l'utente scrive "SOMMA" restituisci in console o tramite alert la somma di tutti i valori inseriti dall'utente.
+
+let numeri = [];
+numeri = (prompt("Inserisci i numeri che vuoi sommare divisi da virgole:")).split(",");
+const azione = prompt("Scrivi 'SOMMA' per procedere:");
+
+
+if (azione.toLowerCase() === "somma") {
+    let somma = 0
+    for (i in numeri) {
+        somma += parseInt(numeri[i]);
+    }
+    console.log(`La somma dei tuoi numeri è ${somma}`);
+}
+
+
 
 // Dati due array, realizza uno script che mostri in console gli elementi comuni ai due array.
 
